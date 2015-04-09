760987a0160602e2d7fd514133ca6c2ddc7c0f3f
walter stylesenberg <stylesenberg@walters-MacBook-Air.local>
2015-04-09 20:59:53 +0200

    renamed variables

--- js/my_javascripts.js
+++ js/my_javascripts.js
@@ -1,135 +1,193 @@
 /*
  * create a function to shuffle an Array
 */
-function shuffle(o){ //v1.0
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+function shuffle(o) { //v1.0
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 };
 
+
 var deck = [];
 
-function shuffle_the_deck () {
-	deck = [];
+function shuffleNewDeck() {
+    deck = [];
 
-	for (var m = 0; m < 6; m++) {
-		for (var i = 0; i < 4; i++) {
-			deck.push(2,3,4,5,6,7,8,9,10,10,10,10,11);
-		};
-	};
-	shuffle(deck);
-	console.log("* ----- A new deck of cards has been shuffled. ----- *");
+    for (var m = 0; m < 6; m++) {
+        for (var i = 0; i < 4; i++) {
+            deck.push(2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, "A");
+        };
+    };
+    shuffle(deck);
+    console.log("* ----- A new deck of cards has been shuffled. ----- *");
+
+    dealInitialCards();
 };
 
-var dealer_hand = [];
-var player_hand = [];
 
-function deal_cards () {
-	player_hand = [];
-	dealer_hand = [];
+var dealersHand = [];
+var playersHand = [];
 
-	player_hand.push(deck.pop());
-	dealer_hand.push(deck.pop());
-	player_hand.push(deck.pop());
+function dealInitialCards() {
+    playersHand = [];
+    dealersHand = [];
 
-	console.log(player_hand);
-	console.log(dealer_hand);
+    playersHand.push(deck.pop());
+    dealersHand.push(deck.pop());
+    playersHand.push(deck.pop());
+
+    console.log(playersHand);
+    console.log(dealersHand);
+    console.log("Initial cards are dealt.");
+
+    calculateValueOfPlayersHand();
+    calculateValueOfDealersHand();
 };
+
 
 var valueOfDealersHand = 0;
 var valueOfPlayersHand = 0;
 
-function calculate_value_of_players_hands () {
+function calculateValueOfPlayersHand() {
 
-	valueOfPlayersHand = 0;
+    valueOfPlayersHand = 0;
 
-	for(var i = 0; i < player_hand.length; i++){
-		valueOfPlayersHand += player_hand[i];
-	};
+    for (var i = 0; i < playersHand.length; i++) {
+        if (playersHand[i] == "A") {
+            valueOfPlayersHand += 1;
+        } else {
+            valueOfPlayersHand += playersHand[i];
+        };
+    };
 
-	if (valueOfPlayersHand == 21) {
-		console.log("--- Player has won! Player has BLACKJACK.")
-	};
+    // calculate the Ace correctly
+    if ((valueOfPlayersHand < 12) && (playersHand.indexOf("A") != -1)) {
+        valueOfPlayersHand += 10;
+    };
 
-	if (valueOfPlayersHand < 21) {
-		console.log("Player has now: " + valueOfPlayersHand);
-	};
+    // announce Blackjack
+    if (valueOfPlayersHand == 21 && playersHand.length < 3) {
+        console.log("----- Player has won! Player shows BLACKJACK.");
+        startNewGame();
+    };
 
-	if (valueOfPlayersHand > 21) {
-		console.log("--- Player has lost the game! Player has " + valueOfPlayersHand);
-	};
+    if (valueOfPlayersHand == 21 && playersHand.length >= 3) {
+        console.log("Player shows: " + valueOfPlayersHand);
+    };
 
-}
+    if (valueOfPlayersHand < 21) {
+        console.log("Player shows: " + valueOfPlayersHand);
+    };
 
-function calculate_value_of_dealers_hands () {
-
-	valueOfDealersHand = 0;
-
-	for (var i = 0; i < dealer_hand.length; i++){
-		valueOfDealersHand += dealer_hand[i];
-	};
-
-	console.log("Dealer has now: " + valueOfDealersHand);
-}
-
-function deal_player_another_card () {
-	var the_new_card = deck.pop();
-	
-	player_hand.push(the_new_card);
-	
-	console.log("Player was dealt a " + the_new_card);
-
-	calculate_value_of_players_hands()
-}
-
-function compare_palyer_and_dealer () {
-	if (valueOfDealersHand < valueOfPlayersHand) {
-		console.log("--- Congratulations, Player has won!")
-	};
-	if (valueOfPlayersHand < valueOfDealersHand && valueOfDealersHand <= 21) {
-		console.log("--- You have LOST!")
-	};
-	if (valueOfPlayersHand == valueOfDealersHand) {
-		console.log("--- It's a draw. Player has " + valueOfPlayersHand + " and Dealer has " + valueOfDealersHand + " as well.")
-	};
-	if (valueOfDealersHand > 21){
-		console.log("--- Dealer busted. Player wins!")
-	}
-}
-
-function dealers_turn () {
-	console.log("Dealer's turn starts ... ");
-	console.log("Dealer currently stands at " + valueOfDealersHand);
-
-	do {
-		var the_new_card = deck.pop();
-
-		dealer_hand.push(the_new_card);
-
-		console.log("Dealer was dealt a " + the_new_card);
-
-		calculate_value_of_dealers_hands();
-	}
-	while(valueOfDealersHand < 17);
-
-	compare_palyer_and_dealer();
+    if (valueOfPlayersHand > 21) {
+        console.log("----- Player has lost! Player shows: " + valueOfPlayersHand);
+        startNewGame();
+    };
 }
 
 
-function start_the_blackjack_game () {
-	shuffle_the_deck();
-	deal_cards();
-	calculate_value_of_players_hands();
-	calculate_value_of_dealers_hands();
+function calculateValueOfDealersHand() {
+
+    valueOfDealersHand = 0;
+
+    for (var i = 0; i < dealersHand.length; i++) {
+        if (dealersHand[i] == "A"){
+            valueOfDealersHand += 1;
+        } else {
+            valueOfDealersHand += dealersHand[i];
+        };
+    };
+
+    // calculate the Ace correctly
+    if ((valueOfDealersHand < 12) && (dealersHand.indexOf("A") != -1)) {
+        valueOfDealersHand += 10;
+    };
+
+    console.log("Dealer has now: " + valueOfDealersHand);
+}
+
+
+function deal_player_another_card() {
+
+    if (valueOfPlayersHand > 20) {
+        console.log("Player has already enough. ;-)");
+        return
+    } else {
+        var the_new_card = deck.pop();
+
+        playersHand.push(the_new_card);
+
+        console.log("Player was dealt a " + the_new_card);
+
+        calculateValueOfPlayersHand()
+    };
+
+}
+
+
+function beginDealersTurn() {
+    console.log("Dealer's turn starts ... ");
+    console.log("Dealer currently stands at " + valueOfDealersHand);
+
+    do {
+        var the_new_card = deck.pop();
+
+        dealersHand.push(the_new_card);
+
+        console.log("Dealer was dealt a " + the_new_card);
+
+        calculateValueOfDealersHand();
+    }
+    while (valueOfDealersHand < 17);
+
+    compareBothHands();
+}
+
+
+function compareBothHands() {
+    if (valueOfDealersHand < valueOfPlayersHand) {
+        console.log("----- Congratulations, Player has won!");
+        startNewGame();
+    };
+    if (valueOfPlayersHand < valueOfDealersHand && valueOfDealersHand <= 21) {
+        console.log("----- You have LOST!");
+        startNewGame();
+    };
+    if (valueOfPlayersHand == valueOfDealersHand) {
+        console.log("----- It's a draw. Player has " + valueOfPlayersHand + " and Dealer has " + valueOfDealersHand + " as well.")
+        startNewGame();
+    };
+    if (valueOfDealersHand > 21) {
+        console.log("----- Dealer busted. Player wins!")
+        startNewGame();
+    }
+}
+
+
+function startNewGame() {
+    console.log("Number of cards in the current deck: " + deck.length);
+    dealInitialCards();
+
+    if (deck.length > 20) {
+
+    } else {
+        shuffleNewDeck()
+    };
+}
+
+
+function startBlackjackGame() {
+    shuffleNewDeck();
 };
 
-$("#btn_start_function").click(function(){
-	start_the_blackjack_game();
+
+$("#btn_start_function").click(function () {
+    startBlackjackGame();
 });
 
-$("#btn_hit_function").click(function(){
-	deal_player_another_card();
+$("#btn_hit_function").click(function () {
+    deal_player_another_card();
 });
 
-$("#btn_stand_function").click(function(){
-	dealers_turn();
+$("#btn_stand_function").click(function () {
+    beginDealersTurn();
 });
